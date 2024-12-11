b736e4154d9b4425ad7647219293fa67a7291253
Gareth NORMAN <norman.gareth@protonmail.com>
2024-12-11 13:36:19 +0800

    User controller and service modification

--- src/main/java/com/oc/PayMyBuddy/controller/UserController.java
+++ src/main/java/com/oc/PayMyBuddy/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
 
     @GetMapping
     public List<User> getAllUsers() {
-        return service.getAllUsers();
+        return UserService.getAllUsers();
     }
 
     @PostMapping
--- src/main/java/com/oc/PayMyBuddy/service/UserService.java
+++ src/main/java/com/oc/PayMyBuddy/service/UserService.java
@@ -10,8 +10,11 @@ import java.util.List;
 
 @Service
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
