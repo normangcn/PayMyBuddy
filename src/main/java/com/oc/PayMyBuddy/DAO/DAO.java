bf849696ef431be86334849d514378e89f036cd3
Gareth NORMAN <norman.gareth@protonmail.com>
2025-04-22 19:10:26 +0800

    Setting up user registration 5 DAO implementation start

---
+++ src/main/java/com/oc/PayMyBuddy/DAO/DAO.java
@@ -0,0 +1,14 @@
+package com.oc.PayMyBuddy.DAO;
+
+public interface DAO<T> {
+
+    Optional<T> get(long id);
+
+    List<T> getAll();
+
+    void save(T t);
+
+    void update(T t, String[] params);
+
+    void delete(T t);
+}
